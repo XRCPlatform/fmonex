a4facdbf18266a7c72615a3af39a6eabe209609b
devbtr1 <henry@bitcoinrh.org>
2019-07-24 15:09:20 +0200

    Avalonia MultiGUI library integration

---
+++ FreeMarketApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace FreeMarketApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}

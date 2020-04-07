dfdbc4b0f3249a4cf50921b3b829193b130a0948
DevRhodium2 <DevRhodium2>
2020-04-07 23:18:30 +0100

    Added Item Price Manager implementation

---
+++ FreeMarketOne.DataStructure.Test/ItemPriceManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using FreeMarketOne.DataStructure.Price;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeMarketOne.DataStructure.Test
+{
+    [TestClass]
+    public class ItemPriceManagerTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void GetExchangeAmount_WithBelowMinAmount()
+        {
+            IMarketItemPrice changellyMgr = new ChangellyItemPriceManager(1.99, new MainConfiguration());
+            var resp = changellyMgr.GetItemPriceInExchangedCurrency
+                (
+                    new Currency[]
+                    {
+                        Currency.BTC,
+                        Currency.USDT
+                    }
+                );
+
+            foreach (var item in resp)
+            {
+                Assert.IsTrue(item.MinAmount>0);
+                Assert.IsTrue(item.Rate > 0);
+            }
+               
+        }
+
+        [TestMethod]
+        public void GetExchangeAmount()
+        {
+            IMarketItemPrice changellyMgr = new ChangellyItemPriceManager(8.99, new MainConfiguration());
+            var resp = changellyMgr.GetItemPriceInExchangedCurrency
+                (
+                    new Currency[]
+                    {
+                        Currency.BTC,
+                        Currency.USDT
+                    }
+                );
+
+            foreach (var item in resp)
+            {
+                Assert.IsTrue(item.MinAmount > 0);
+                Assert.IsTrue(item.Rate > 0);
+            }
+
+        }
+    }
+}
--- FreeMarketOne.DataStructure/Price/ChangellyItemPriceManager.cs
+++ FreeMarketOne.DataStructure/Price/ChangellyItemPriceManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FreeMarketOne.DataStructure.Price.ChangellyApi;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,16 +8,55 @@ namespace FreeMarketOne.DataStructure.Price
     public class ChangellyItemPriceManager : IMarketItemPrice
     {
         private double basePrice;
-        public ChangellyItemPriceManager(double basePriceInXrc)
+        
+        //need singleton for api so that caches and etc are preserved 
+        private static ChangellyApiClient changellyApiClient;
+        private readonly object constructorLock = new object();
+        
+        public ChangellyItemPriceManager(double basePriceInXrc, IBaseConfiguration config)
         {
             basePrice = basePriceInXrc;
+            lock (constructorLock)
+            {
+                if (changellyApiClient == null)
+                {
+                    changellyApiClient = new ChangellyApiClient(config);
+                }
+            }
         }
 
         public double BasePrice { get { return basePrice; } }
 
-        public IEnumerable<IItemPriceResponse> GetItemPriceInExchangedCurrency(Currency[] currency)
+        public IEnumerable<IItemPriceResponse> GetItemPriceInExchangedCurrency(Currency[] currencies)
         {
-            throw new NotImplementedException();
+            List<ItemPriceResponse> itemPrices = new List<ItemPriceResponse>();
+            var mins = changellyApiClient.GetMinAmount(Currency.XRC, currencies).result;
+            var response = changellyApiClient.GetExchangeAmount(Currency.XRC, currencies, basePrice);
+            foreach (var item in response.result)
+            {
+                double minAmount = 0;
+                foreach (var min in mins)
+                {
+                    if(min.To.Equals(item.To, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        minAmount = min.MinAmount;
+                        break;
+                    }
+                }
+                object exCurrency = null; 
+                if (Enum.TryParse(typeof(Currency), item.To.ToUpper(), out exCurrency))
+                {
+                    itemPrices.Add(new ItemPriceResponse()
+                    {
+                        Amount = item.Amount,
+                        Currency = (Currency) exCurrency,
+                        Fee = item.Fee,
+                        Rate = item.Rate,
+                        MinAmount = minAmount
+                    });
+                }                
+            }
+            return itemPrices;
         }
     }
 }

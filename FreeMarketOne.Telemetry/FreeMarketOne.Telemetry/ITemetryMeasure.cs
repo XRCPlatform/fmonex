726923dd47ab3e6d20fa9f320bcf77a10209ea01
DevRhodium2 <DevRhodium2>
2020-04-21 22:23:37 +0100

    Updated telemetry interface

--- FreeMarketOne.Telemetry/FreeMarketOne.Telemetry/ITemetryMeasure.cs
+++ FreeMarketOne.Telemetry/FreeMarketOne.Telemetry/ITemetryMeasure.cs
@@ -2,36 +2,36 @@
 
 namespace FreeMarketOne.Telemetry
 {
-    public class ITemetryMeasure
+    interface ITemetryMeasure
     {
         /// <summary>
         /// Date and time of the measurable event.
         /// </summary>
-        public DateTime Time { get; set; }
+        DateTime Time { get; set; }
 
         /// <summary>
         /// Platform version so that we can monitor upgrades and compatibilities.
         /// </summary>
-        public string PlatformVersion { get; set; }
+        string PlatformVersion { get; set; }
 
         /// <summary>
         /// Anonymised user hash solely used for measure but not revealing anything personal such us IPs, identities and etc.
         /// </summary>
-        public string UserId { get; set; }
+        string UserId { get; set; }
 
         /// <summary>
         /// Action that user has taken, such as start_app, stop_app, search, bid, list_product, buy_product, view_product, expand_product, request_arbitration and etc. 
         /// </summary>
-        public string Action { get; set; }
+        string Action { get; set; }
 
         /// <summary>
         /// A thing that is acted on, such as product id, search phrase and etc.
         /// </summary>
-        public string ActionTarget { get; set; }
+        string ActionTarget { get; set; }
 
         /// <summary>
         /// Result of the action. Such as succesfuly completed transaction. Arbitration. Win/Lose bid.
         /// </summary>
-        public string Outcome { get; set; }
+        string Outcome { get; set; }
     }
 }

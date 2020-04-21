9e195fbc556997fe811d76c914b93e32f584d75e
DevRhodium2 <DevRhodium2>
2020-04-21 23:44:37 +0100

    Added elastic recommended request headers

--- FreeMarketOne.Telemetry/FreeMarketOne.Telemetry/ElasticTelemetryServer.cs
+++ FreeMarketOne.Telemetry/FreeMarketOne.Telemetry/ElasticTelemetryServer.cs
@@ -21,6 +21,9 @@ namespace FreeMarketOne.Telemetry
             HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, this.configuration.TelemetryServerUri);
             httpRequest.Content = new JsonContent(measure);
             httpRequest.Headers.TryAddWithoutValidation("Content-Type", "application/json");
+            //recomended by elastic client
+            httpRequest.Headers.TransferEncodingChunked = false;
+            httpRequest.Headers.ConnectionClose = false;                
             //deliberate fire and forget to avoid negatively impacting ux
             await httpClient.SendAsync(httpRequest).ConfigureAwait(false);
         }

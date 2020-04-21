c8dfd199d06d2d176b0652824036e8d2bd5d305c
DevRhodium2 <DevRhodium2>
2020-04-21 22:51:30 +0100

    Added telemetry server.

---
+++ FreeMarketOne.Telemetry/FreeMarketOne.Telemetry/ITelemetryServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeMarketOne.Telemetry
+{
+    interface ITelemetryServer
+    {
+        /// <summary>
+        /// Send telemetry data to server.
+        /// </summary>
+        /// <param name="measure"></param>
+        void Send(ITemetryMeasure measure);
+    }
+}

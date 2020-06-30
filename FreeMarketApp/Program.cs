5e1646bf2b31eac148ae19deba11f785efdaf672
devbtr1 <henry@bitcoinrh.org>
2020-06-30 22:13:48 +0200

    Added icon to exe file

--- FreeMarketApp/Program.cs
+++ FreeMarketApp/Program.cs
@@ -13,7 +13,7 @@ namespace FreeMarketApp
             Console.WriteLine("FREEMARKET.ONE desktop application;");
             Console.WriteLine("Copyright (c) 2020 www.freemarket.one;");
             Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY;");
-            Console.WriteLine("This is free software, and you are welcome to redistribute it under GNU GPL license;");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it;");
             Console.WriteLine("------------------------------------");
 
             BuildAvaloniaApp()

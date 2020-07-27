431c8c218791d9c55d490bce0e5d9d60b06475bb
devbtr1 <henry@bitcoinrh.org>
2020-07-27 10:46:28 +0200

    Added example of ServerLoadedEvent usage for test network

--- FreeMarketApp/App.xaml.cs
+++ FreeMarketApp/App.xaml.cs
@@ -3,14 +3,20 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using FreeMarketApp.ViewModels;
 using FreeMarketApp.Views;
+using FreeMarketOne.DataStructure;
+using FreeMarketOne.DataStructure.Objects.BaseItems;
 using FreeMarketOne.ServerCore;
+using Libplanet.Blockchain;
 using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FreeMarketApp
 {
     public class App : Application
     {
+        private static bool _newBlock = false;
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -57,6 +63,7 @@ namespace FreeMarketApp
         {
             await Task.Run(() =>
             {
+                FreeMarketOneServer.Current.BaseBlockChainChangedEvent += new EventHandler<BlockChain<BaseAction>.TipChangedEventArgs>(BaseBlockChainChanged);
                 FreeMarketOneServer.Current.FreeMarketOneServerLoadedEvent += ServerLoadedEvent;
                 FreeMarketOneServer.Current.Initialize();
             }).ConfigureAwait(true);
@@ -64,9 +71,45 @@ namespace FreeMarketApp
             return new MainWindow { DataContext = new MainWindowViewModel() };
         }
 
+        private static void BaseBlockChainChanged(object sender, EventArgs e)
+        {
+            //we have a new block
+            _newBlock = true;
+        }
+
         private static void ServerLoadedEvent(object sender, EventArgs e)
         {
-            //do activities after load;
+            //var testActionItem2 = new ReviewUserDataV1();
+            //testActionItem2.ReviewDateTime = DateTime.UtcNow.AddMinutes(-1);
+            //testActionItem2.Message = "This is a test message";
+            //testActionItem2.Hash = testActionItem2.GenerateHash();
+
+            //FreeMarketOneServer.Current.BasePoolManager.AcceptActionItem(testActionItem2);
+
+            ////complete tx and send it to network
+            //SpinWait.SpinUntil(() => FreeMarketOneServer.Current.BaseBlockChainManager.SwarmServer.Running);
+            //FreeMarketOneServer.Current.BasePoolManager.PropagateAllActionItemLocal();
+
+            ////now waiting for mining
+            //SpinWait.SpinUntil((() => FreeMarketOneServer.Current.BasePoolManager.GetAllActionItemLocal().Count == 0));
+
+            ////now wait until mining will start
+            //SpinWait.SpinUntil(() => FreeMarketOneServer.Current.BasePoolManager.IsMiningWorkerRunning());
+
+            ////now wait until we havent a new block
+            //SpinWait.SpinUntil((() => _newBlock == true));
+
+            ////wait until end of mining
+            //SpinWait.SpinUntil(() => !FreeMarketOneServer.Current.BasePoolManager.IsMiningWorkerRunning());
+
+            //var hashSets = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.IterateBlockHashes().ToHashSet();
+
+            //var chainId = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.GetCanonicalChainId();
+            //var block0HashId = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.IndexBlockHash(chainId.Value, 0);
+            //var block1HashId = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.IndexBlockHash(chainId.Value, 1);
+
+            //var blockO = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.GetBlock<BaseAction>(block0HashId.Value);
+            //var block1 = FreeMarketOneServer.Current.BaseBlockChainManager.Storage.GetBlock<BaseAction>(block1HashId.Value);
         }
 
         private static void OnExit(object sender, EventArgs e)

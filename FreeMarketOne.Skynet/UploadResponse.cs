a25a8ae18f0351700d6a2032327c7166dcfe007a
devbtr1 <henry@bitcoinrh.org>
2020-08-07 14:12:30 +0200

    Sia Skynet blockchain image storage connector

---
+++ FreeMarketOne.Skynet/UploadResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace FreeMarketOne.Skynet
+{
+    /// <summary>
+    /// Successful upload response from the webportal
+    /// </summary>
+    public class UploadResponse
+    {
+        /// <summary>
+        /// Skylink to access the upload content
+        /// </summary>
+        public string Skylink { get; set; }
+
+        /// <summary>
+        /// The hash that is encoded into the Skylink
+        /// </summary>
+        public string Merkleroot { get; set; }
+
+        /// <summary>
+        /// The bitfield contains a version, an offset and a length in a heavily compressed and optimized format
+        /// </summary>
+        public ushort Bitfield { get; set; }
+    }
+}

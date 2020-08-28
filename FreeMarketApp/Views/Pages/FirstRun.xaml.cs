b12ecce70fceb7954be0b7e72729d1eec5ec4728
devbtr1 <henry@bitcoinrh.org>
2020-08-28 14:54:14 +0200

    Added server reloading with splash window after setup of new user private key

--- FreeMarketApp/Views/Pages/FirstRun.xaml.cs
+++ FreeMarketApp/Views/Pages/FirstRun.xaml.cs
@@ -2,7 +2,9 @@
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using FreeMarketApp.Helpers;
+using FreeMarketApp.ViewModels;
 using FreeMarketOne.ServerCore;
+using System.Threading.Tasks;
 
 namespace FreeMarketApp.Views.Pages
 {
@@ -37,7 +39,87 @@ namespace FreeMarketApp.Views.Pages
         {
             var mainWindow = PagesHelper.GetParentWindow(this);
 
-            PagesHelper.Switch(mainWindow, MyProfilePage.Instance);
+            ////check form
+            //var tbTitle = this.FindControl<TextBox>("TBTitle");
+            //var tbDescription = this.FindControl<TextBox>("TBDescription");
+            //var tbShipping = this.FindControl<TextBox>("TBShipping");
+            //var cbCategory = this.FindControl<ComboBox>("CBCategory");
+            //var cbDealType = this.FindControl<ComboBox>("CBDealType");
+
+            //var errorCount = 0;
+
+            //if (string.IsNullOrEmpty(tbTitle.Text)) errorCount++;
+            //if (string.IsNullOrEmpty(tbDescription.Text)) errorCount++;
+            //if (string.IsNullOrEmpty(tbShipping.Text)) errorCount++;
+
+            //var cbCategoryValue = cbCategory.SelectedItem as ComboBoxItem;
+            //if (cbCategoryValue.Tag.ToString() == "0") errorCount++;
+
+            //var cbDealTypeValue = cbDealType.SelectedItem as ComboBoxItem;
+            //if (cbDealTypeValue.Tag.ToString() == "0") errorCount++;
+
+            //if (_marketItemData.Photos.Count() == 0) errorCount++;
+
+            //if (errorCount == 0)
+            //{
+            //    //save to chain
+            //    _marketItemData.Title = tbTitle.Text;
+            //    _marketItemData.Description = tbDescription.Text;
+            //    _marketItemData.Shipping = tbShipping.Text;
+            //    _marketItemData.Category = cbCategoryValue.Tag.ToString();
+            //    _marketItemData.DealType = cbDealTypeValue.Tag.ToString();
+
+            //    //get time to next block
+            //    //upload to sia
+            //    for (int i = _marketItemData.Photos.Count(); i > 0; i--)
+            //    {
+            //        if (!_marketItemData.Photos[i - 1].Contains(SkynetWebPortal.SKYNET_PREFIX))
+            //        {
+            //            var skynetUrl = UploadToSkynet(_marketItemData.Photos[i - 1]);
+            //            if (skynetUrl == null)
+            //            {
+            //                _marketItemData.Photos.RemoveAt(i - 1);
+            //            }
+            //            else
+            //            {
+            //                _marketItemData.Photos[i - 1] = skynetUrl;
+            //            }
+            //        }
+            //    }
+
+            //    PagesHelper.Switch(mainWindow, MyProductsPage.Instance);
+            //    ClearForm();
+
+            //}
+            //else
+            //{
+
+            //    await MessageBox.Show(mainWindow,
+            //       SharedResources.ResourceManager.GetString("Dialog_AddEditProduct_EmptyForm"),
+            //        SharedResources.ResourceManager.GetString("Dialog_Information_Title"),
+            //        MessageBox.MessageBoxButtons.Ok);
+            //}
+
+
+            async void AppAsyncLoadingStart()
+            {
+                var splashViewModel = new SplashWindowViewModel();
+                splashViewModel.StartupProgressText = "Reloading...";
+                var splashWindow = new SplashWindow { DataContext = splashViewModel };
+                splashWindow.Show();
+                await Task.Delay(10);
+
+                //reloading server with splash window
+                FreeMarketOneServer.Current.Initialize();
+                PagesHelper.Switch(mainWindow, MainPage.Instance);
+
+                if (splashWindow != null)
+                {
+                    splashWindow.Close();
+                }
+            }
+
+            AppAsyncLoadingStart();
         }
 
         public void ButtonRandomSeed_Click(object sender, RoutedEventArgs args)

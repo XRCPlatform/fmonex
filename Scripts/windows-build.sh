29f6f6a69b9e5d7c91b90a813e3ebc9a4151d249
DevRhodium 3 <dvbr3@protonmail.com>
2020-09-20 11:09:49 +0200

    Add first version of windows installer scripts

---
+++ Scripts/windows-build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Ensure you have wine32 installed.
+
+set -e
+
+dotnet_runtime="win-x64"
+warp_runtime="win-x64"
+configuration="release"
+git_commit=$(git log --format=%h --abbrev=7 -n 1)
+publish_directory="../FreeMarketApp/bin/${configuration}/netcoreapp3.1/${dotnet_runtime}/publish"
+download_directory="/tmp"
+project_path="../FreeMarketApp/FreeMarketApp.csproj"
+
+version=0.0.1
+
+. common.sh
+
+#######
+
+nsis_filename=nsis-3.06.1-setup.exe
+nsis_url=https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06.1/$nsis_filename
+nsis_sha256=f60488a676308079bfdf6845dc7114cfd4bbff47b66be4db827b89bb8d7fdc52
+cache_dir=./tmp-tools
+export WINEPREFIX=$(pwd)/wine
+export WINEDEBUG=-all
+
+mkdir -p $WINEPREFIX
+
+wine 'wineboot'
+mkdir -p $cache_dir
+
+pushd $cache_dir
+if [ ! -f $nsis_filename ]; then
+   wget $nsis_url
+fi
+popd
+
+mkdir -p $WINEPREFIX/drive_c/fmone
+cp $publish_directory/* $WINEPREFIX/drive_c/fmone
+# TODO: need to include ico
+cp fmone.ico $WINEPREFIX/drive_c/fmone/fmone.ico
+
+echo "$nsis_sha256 $cache_dir/$nsis_filename" | if sha256sum --check ; then
+    echo "Building with nsis"
+    if [ ! -f "$WINEPREFIX/drive_c/Program Files/NSIS/makensis.exe" ]; then
+        wine "$cache_dir/$nsis_filename"
+    fi
+    wine "$WINEPREFIX/drive_c/Program Files/NSIS/makensis.exe" /DPRODUCT_VERSION=$version fmone.nsi
+else
+    echo "nsis file was not the correct checksum" >&2
+fi

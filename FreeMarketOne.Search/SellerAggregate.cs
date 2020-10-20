a9252090d9f256380f3e2b985b2ad76568e3b61d
DevRhdhodium2 <rodendron@bitcoinrh.org>
2020-10-20 20:17:47 +0100

    Created seller aggregate entity

---
+++ FreeMarketOne.Search/SellerAggregate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeMarketOne.Search
+{
+    public class SellerAggregate
+    {
+        public double TotalXRCVolume { get; set; }
+        public int StarRating { get; set; }
+        public List<string> PublicKeyHashes { get; set; }
+        public string SellerName { get; set; }
+
+    }
+}

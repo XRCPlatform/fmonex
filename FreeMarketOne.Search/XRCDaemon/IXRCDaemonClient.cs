6872f32cecc61443eae0c23fc28992361b060c13
DevRhdhodium2 <rodendron@bitcoinrh.org>
2020-10-25 00:16:28 +0100

    Adding XRCDeaemonClient interface for testability and moqs.

---
+++ FreeMarketOne.Search/XRCDaemon/IXRCDaemonClient.cs
@@ -0,0 +1,10 @@
+﻿using FreeMarketOne.Search.XRCDaemon;
+using System.Threading.Tasks;
+
+namespace FreeMarketOne.Search
+{
+    public interface IXRCDaemonClient
+    {
+        Task<TransactionVerboseModel> GetTransaction(string transactionHash);
+    }
+}

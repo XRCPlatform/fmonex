5fe5e50fed32431f70643afbd6dcc257626d18d1
srgkrslnk <sergey.xrc@icloud.com>
2020-12-06 12:14:45 +0200

    popup

--- FreeMarketApp/Views/MainWindow.xaml.cs
+++ FreeMarketApp/Views/MainWindow.xaml.cs
@@ -138,7 +138,7 @@ namespace FreeMarketApp.Views
                 SearchResultsPage.SetSearchPhrase(searchText);
 
                 PagesHelper.Switch(this, SearchResultsPage.Instance);
-            }            
+            }
         }
 
         public void ButtonMyProfile_Click(object sender, RoutedEventArgs args)
@@ -150,7 +150,8 @@ namespace FreeMarketApp.Views
         {
             PagesHelper.Log(_logger, "ServerLoadedEvent on MainWindow was raised.");
 
-            Dispatcher.UIThread.InvokeAsync(() => { 
+            Dispatcher.UIThread.InvokeAsync(() =>
+            {
                 PagesHelper.SetUserData(_logger, this);
                 PagesHelper.SetServerData(_logger, this);
             });
@@ -159,21 +160,25 @@ namespace FreeMarketApp.Views
         public void ButtonSettings_Click(object sender, RoutedEventArgs args)
         {
             var settingsPage = SettingsPage.Instance;
-            
+
             Panel panel = this.FindControl<Panel>("PCMainContent");
-            if (panel.Children.Any()) {
+            if (panel.Children.Any())
+            {
 
                 var pageInstance = panel.Children.Last();
 
-                if (pageInstance.GetType() == typeof(AddEditProductPage)) {
+                if (pageInstance.GetType() == typeof(AddEditProductPage))
+                {
                     settingsPage.SetReturnTo(MyProductsPage.GetInstance());
-                } 
-                else if (pageInstance.GetType() == typeof(EditProfilePage)) {
+                }
+                else if (pageInstance.GetType() == typeof(EditProfilePage))
+                {
                     settingsPage.SetReturnTo(MyProfilePage.GetInstance());
                 }
-                else if (pageInstance.GetType() == typeof(SearchResultsPage)) {
+                else if (pageInstance.GetType() == typeof(SearchResultsPage))
+                {
                     settingsPage.SetReturnTo(MainPage.GetInstance());
-                } 
+                }
                 else
                 {
                     settingsPage.SetReturnTo((UserControl)panel.Children.Last());
@@ -181,6 +186,15 @@ namespace FreeMarketApp.Views
             }
 
             PagesHelper.Switch(this, settingsPage);
+        }
+
+        public void ButtonPopup_Click(object sender, RoutedEventArgs args)
+        {
+            var popbtn = sender as Button;
+            var popup = this.Popup;
+            if (popup.IsOpen) { popup.IsOpen = false; }
+            popup.PlacementTarget = popbtn;
+            popup.IsOpen = true;
         }
     }
 }

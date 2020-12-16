9795e2d9af1c9cf7713cc4c7490896d50417e46a
srgkrslnk <sergey.xrc@icloud.com>
2020-12-16 13:37:08 +0200

    Revert "Fixing IBD indexing issue."
    This reverts commit ac1933cbbafed817723a3e4e9b5ae37948ca8f27.

--- FreeMarketOne.ServerCore/FreeMarketOneServer.cs
+++ FreeMarketOne.ServerCore/FreeMarketOneServer.cs
@@ -147,13 +147,6 @@ namespace FreeMarketOne.ServerCore
                     OnionSeedsManager = new OnionSeedsManager(Configuration, TorProcessManager, ServerPublicAddress.PublicIP);
                     OnionSeedsManager.Start();
 
-                    //Search indexer
-                    LoadingEvent?.Invoke(this, "Loading Local Search Engine...");
-                    XRCDaemonClient client = new XRCDaemonClient(new JsonSerializerSettings(), Configuration, _logger);
-                    SearchIndexer = new SearchIndexer(Markets, Configuration, new XRCHelper(client), Users, BasePoolManager, BaseBlockChainManager);
-                    SearchIndexer.Initialize();
-                    SearchEngine = new SearchEngine(Markets, SearchHelper.GetDataFolder(Configuration));
-
                     //Initialize Base BlockChain Manager
                     LoadingEvent?.Invoke(this, "Loading Base BlockChain Manager...");
                     BaseBlockChainLoadEndedEvent += new EventHandler(Current.BaseBlockChainLoaded);
@@ -184,6 +177,12 @@ namespace FreeMarketOne.ServerCore
                         Configuration.BlockChainBasePolicy);
                     BasePoolManager.Start();
 
+                      //Search indexer
+                    LoadingEvent?.Invoke(this, "Loading Local Search Engine...");
+                    XRCDaemonClient client = new XRCDaemonClient(new JsonSerializerSettings(), Configuration, _logger);
+                    SearchIndexer = new SearchIndexer(Markets, Configuration, new XRCHelper(client), Users, BasePoolManager, BaseBlockChainManager);
+                    SearchIndexer.Initialize();
+                    SearchEngine = new SearchEngine(Markets, SearchHelper.GetDataFolder(Configuration));
                 }
                 else
                 {

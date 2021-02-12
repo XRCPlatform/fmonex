758e6578e8bf709aaefe0021d45717fe6a67f8dd
DevRhdhodium2 <rodendron@bitcoinrh.org>
2021-02-12 00:59:43 +0000

    Adding exception for specific error handling.

---
+++ FreeMarketOne.LibPlanet/Libplanet/Net/PooledSocketUnavailableException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Libplanet.Net
+{
+    [Serializable]
+    internal class PooledSocketUnavailableException : Exception
+    {
+        public PooledSocketUnavailableException()
+        {
+        }
+
+        public PooledSocketUnavailableException(string message) : base(message)
+        {
+        }
+
+        public PooledSocketUnavailableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected PooledSocketUnavailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

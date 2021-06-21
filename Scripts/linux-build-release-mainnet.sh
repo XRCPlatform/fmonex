a5cd0d75d7b281bacf0b7acc8d4fd3c655698642
Marcus0x <marcus0x@xrhodium.org>
2021-06-21 09:11:38 +0200

    Fixed release build, created scripts for testnet build (Debug) and mainnet build (Release)

---
+++ Scripts/linux-build-release-mainnet.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+dotnet_runtime="linux-x64"
+warp_runtime="linux-x64"
+configuration="Release"
+git_commit=$(git log --format=%h --abbrev=7 -n 1)
+publish_directory="../FreeMarketApp/bin/${configuration}/netcoreapp3.1/${dotnet_runtime}/publish"
+download_directory="/tmp"
+project_path="../FreeMarketApp/FreeMarketApp.csproj"
+
+. common.sh
+
+#######
+
+# Create a .deb file
+mkdir -p pkg-debian pkg-debian/DEBIAN pkg-debian/usr/bin pkg-debian/usr/share/applications
+touch pkg-debian/DEBIAN/{conffiles,control,md5sums,postinst,prerm} pkg-debian/debian-binary
+chmod 555 pkg-debian/DEBIAN/postinst
+chmod 555 pkg-debian/DEBIAN/prerm
+
+cp ${publish_directory}/FreeMarketApp pkg-debian/usr/bin/freemarketone
+
+cat <<EOF > pkg-debian/DEBIAN/control
+Package: freemarketone
+Version: 1.1
+Architecture: amd64
+Essential: no
+Section: web
+Maintainer: FreeMarketOne Developers
+Installed-Size: 49000
+Description: Decentralized precious metal marketplace.
+EOF
+
+cat <<EOF > pkg-debian/usr/share/applications/freemarketone.desktop
+[Desktop Entry]
+Name=FreeMarketOne
+Comment=Decentralized precious metal marketplace.
+Exec=/usr/bin/freemarketone
+Terminal=false
+Type=Application
+Categories=Unknown
+EOF
+
+cd pkg-debian/
+find . -type f ! -regex '.*.git.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
+cd ..
+dpkg -b pkg-debian freemarketone-1.1_amd64.deb
+
+rm -Rf pkg-debian
+
+echo "Done."
---
+++ Scripts/windows-build-release-mainnet.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Ensure you have wine32 installed.
+
+set -e
+
+dotnet_runtime="win-x64"
+warp_runtime="win-x64"
+configuration="Release"
+git_commit=$(git log --format=%h --abbrev=7 -n 1)
+publish_directory="../FreeMarketApp/bin/${configuration}/netcoreapp3.1/${dotnet_runtime}/publish"
+download_directory="/tmp"
+project_path="../FreeMarketApp/FreeMarketApp.csproj"
+ico_path="../FreeMarketApp/Assets/freemarket.ico"
+
+version="1.1"
+
+. common-windows.sh
+
+#######
+
+nsis_filename=nsis-3.06.1-setup.exe
+nsis_url=https://downloads.sourceforge.net/project/nsis/NSIS%203/3.06.1/$nsis_filename
+nsis_sha256=f60488a676308079bfdf6845dc7114cfd4bbff47b66be4db827b89bb8d7fdc52
+cache_dir=./tmp-tools
+export WINEPREFIX=$(pwd)/wine
+export WINEDEBUG=-all
+
+mkdir -p $WINEPREFIX
+
+wine 'wineboot'
+mkdir -p $cache_dir
+
+pushd $cache_dir
+if [ ! -f $nsis_filename ]; then
+   wget $nsis_url
+fi
+popd
+
+mkdir -p $WINEPREFIX/drive_c/fmone
+cp -f -R $publish_directory/* $WINEPREFIX/drive_c/fmone
+
+echo "$nsis_sha256 $cache_dir/$nsis_filename" | if sha256sum --check ; then
+    echo "Building with nsis"
+    if [ ! -f "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" ]; then
+        wine "$cache_dir/$nsis_filename"
+    fi
+    wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" /DPRODUCT_VERSION=$version fmone-windows.nsi
+else
+    echo "nsis file was not the correct checksum" >&2
+fi
